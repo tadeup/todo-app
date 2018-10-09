6e9f655d49db881ff7c1f6a846f48ec3e67b7e96
unknown <tadeup1@gmail.com>
2018-10-09 12:14:35 -0300

    final commit

--- server/server.js
+++ server/server.js
@@ -135,6 +135,14 @@ app.post('/users/login', (req, res) => {
         
         res.status(400).send();
     });
+});
+
+app.delete('user/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send()
+    })
 })
 
 app.listen(port, () =>{
